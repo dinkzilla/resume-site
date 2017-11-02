20a1c9d6102a690662acc2a332ca09598b5223fd
Matt Dinkel <dinkzilla@gmail.com>
2017-11-02 00:14:53 -0400

    POC - Resume Highlighting
    Adjusted menu puttons and tied to highlighting in page content.
    
    Not Mobile Friendly.
    
    No header or contact info.

--- src/app.ts
+++ src/app.ts
@@ -3,14 +3,13 @@ import { RouterConfiguration, Router } from 'aurelia-router';
 export class App {
   private router;
 
-  public configureRouter(config: RouterConfiguration, router: Router){
+  public configureRouter(config: RouterConfiguration, router: Router) {
     this.router = router;
     config.map([
-      { route: '', name: 'home', moduleId: 'routes/home/home'},
-      { route: 'work', name: 'work', moduleId: 'routes/work/work'},
-      { route: 'dev', name: 'dev', moduleId: 'routes/dev/dev'},
-      { route: 'risk', name: 'risk', moduleId: 'routes/risk/risk'},
-      { route: 'design', name: 'design', moduleId: 'routes/design/design'}
+      { route: '', name: 'clear', moduleId: 'routes/home/home' },
+      { route: 'dev', name: 'dev', moduleId: 'routes/dev/dev' },
+      { route: 'risk', name: 'risk', moduleId: 'routes/risk/risk' },
+      { route: 'design', name: 'design', moduleId: 'routes/design/design' }
     ])
   }
 }
